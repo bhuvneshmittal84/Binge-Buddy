efe0103f3010901e1cddf8ff1a2017d1d191f26c
bhuvneshmittal84 <bhuvneshmittal84@gmail.com>
2026-09-24 19:00:01 +0530

    chore: secure firebase credentials in .env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHD1qbLhEVrpTzl0VJvUMQM5RXJoomRSg",
-  authDomain: "bingebuddy-844ba.firebaseapp.com",
-  projectId: "bingebuddy-844ba",
-  storageBucket: "bingebuddy-844ba.firebasestorage.app",
-  messagingSenderId: "223433085999",
-  appId: "1:223433085999:web:22e220e917e8d6437ce2da",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app  = initializeApp(firebaseConfig);
